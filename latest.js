95be6037f65b058fd452eb10a0bd3e968c970c37
FlawCra <moritz.fuessel@flawcra.cc>
2021-05-08 13:32:34 +0200

    Update latest.js

--- latest.js
+++ latest.js
@@ -1,7 +1,25 @@
-(function() {
-    'use strict';
-    setTimeout(function(){
-        document.querySelector('[aria-label="VERIFICATION"]').parentNode.removeChild(document.querySelector('[aria-label="VERIFICATION"]'));
-        document.querySelector("#app-mount > div:first-of-type").children[0].children[1].children[0].style = "";
-    }, 5000);
-})();
+
+
+    // ==UserScript==
+    // @name         Discord Verify Exploit
+    // @namespace    http://flawcra.cc/
+    // @version      2.0.0
+    // @description  Workaround for the Discord Verification
+    // @author       FlawCra
+    // @match        https://discordapp.com/library/*
+    // @match        https://discordapp.com/store/*
+    // @match        https://discordapp.com/channels/*
+    // @match        https://discord.com/library/*
+    // @match        https://discord.com/store/*
+    // @match        https://discord.com/channels/*
+    // @run-at document-end
+    // ==/UserScript==
+     
+    (function() {
+        'use strict';
+        setTimeout(function(){
+            document.querySelector('[aria-label="VERIFICATION"]').remove();
+            document.querySelector("#app-mount > div:nth-of-type(2)").children[0].children[1].children[0].style = ""
+        }, 15000);
+    })();
+
